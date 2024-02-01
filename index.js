8b215f110ba54298520afb96e805e80b56601f84
Arnold C <78853864+arnoldcuriano@users.noreply.github.com>
2024-02-01 22:30:54 +0800

    Adding style on pressing button using eventlistner

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ for (var i = 0; i < numberOfKeys; i++) {
         
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
        
     });
-}
+}    
   
 
 
 //For keyboard press function
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
 
 function makeSound(key)  {
 
-    switch (key) {
+    switch (key.toLowerCase()) {
         case 'w':
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -59,7 +61,15 @@ function makeSound(key)  {
 }
 
 
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
 
+    setTimeout(function (){
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
 
 //  var audio = new Audio("sounds/tom-1.mp3");
 // audio.play();
